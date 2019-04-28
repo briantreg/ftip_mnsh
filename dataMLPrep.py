ce62d97974683b5cf6f6f75e10011cf2f4ea22d7
briantreg <briantreg@gmail.com>
2019-04-28 17:14:02 +1000

    Added section to incorporate average fantasy scores in model

--- dataMLPrep.py
+++ dataMLPrep.py
@@ -1,6 +1,7 @@
 import pickle
 import dataWrangling as dw
 import utilities.utilitiesML as mlearn
+import pandas as pd
 # =============================================================================
 # Set up the inputs for the modelling
 # =============================================================================
@@ -21,7 +22,43 @@ target = 'model_target'
 # =============================================================================
 # Import data, define predictor columns and split test and train
 # =============================================================================
-afl_ML, afl_DF = dw.dataWrangling(new_data = '', scoring_data = False)
+afl_ML, afl_DF = dw.dataWrangling(new_data = '', outputType = '')
+dfFantasyAverage = pd.read_pickle('data/dfFantasyAverage.pkl')
+
+###Add in fantasy###
+afl_ML['fw_game_id'] = afl_ML['fw_game_id'].astype('int')
+dfFantasyAverage_ML = afl_ML[['fw_game_id','team','opponent']]
+
+dfFantasyAverage_team = dfFantasyAverage
+dfFantasyAverage_team.columns =['fw_game_id', 'team', 'supercoachavg_team', 'aflfantasyavg_team']
+dfFantasyAverage_ML = pd.merge(dfFantasyAverage_ML ,
+      dfFantasyAverage_team,
+      left_on = ['team','fw_game_id'],
+      right_on = ['team','fw_game_id'],
+      how = 'left')
+
+dfFantasyAverage_opponent = dfFantasyAverage
+dfFantasyAverage_opponent.columns =['fw_game_id', 'opponent', 'supercoachavg_opponent', 'aflfantasyavg_opponent']
+dfFantasyAverage_ML = pd.merge(dfFantasyAverage_ML,
+      dfFantasyAverage_opponent,
+      left_on = ['opponent','fw_game_id'],
+      right_on = ['opponent','fw_game_id'],
+      how = 'left')
+
+dfFantasyAverage_ML['fantasyavg_team'] = dfFantasyAverage_ML['aflfantasyavg_team'] + dfFantasyAverage_ML['supercoachavg_team']
+dfFantasyAverage_ML['fantasyavg_opponent'] = dfFantasyAverage_ML['aflfantasyavg_opponent'] + dfFantasyAverage_ML['supercoachavg_opponent']
+
+dfFantasyAverage_ML['SupercoachAvg_D'] = (dfFantasyAverage_ML['supercoachavg_team'] / (dfFantasyAverage_ML['supercoachavg_team'] + dfFantasyAverage_ML['supercoachavg_opponent']))
+dfFantasyAverage_ML['AflFantasyAvg_D'] = (dfFantasyAverage_ML['aflfantasyavg_team'] / (dfFantasyAverage_ML['aflfantasyavg_team'] + dfFantasyAverage_ML['aflfantasyavg_opponent']))
+dfFantasyAverage_ML['FantasyAvg_D'] = (dfFantasyAverage_ML['fantasyavg_team'] / (dfFantasyAverage_ML['fantasyavg_team'] + dfFantasyAverage_ML['fantasyavg_opponent']))
+
+dfFantasyAverage_ML = dfFantasyAverage_ML[['fw_game_id','team', 'FantasyAvg_D']]
+afl_ML = pd.merge(afl_ML,
+         dfFantasyAverage_ML,
+         on = ['fw_game_id','team'],
+         how = 'left')
+
+###Define ML Data###
 X_cols = mlearn.defineMLData(afl_ML, target, M_cols)
 test_ids, train, test = mlearn.splitData(afl_ML, test_ids_file)
 afl_ML.columns
@@ -46,3 +83,4 @@ objectsScore = (X_cols, X_scaler, feat_selector)
 pickle.dump(objectsScore , open("objectsScore.pkl", 'wb'))
 
 pickle.dump(feat_selector, open("featureSelect.pkl", 'wb'))
+
